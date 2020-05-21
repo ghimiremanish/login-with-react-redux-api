56bba1dc433b47d6735f9e495486dbb75339a816
Manish Ghimire <algorithm@Algorithm-2.local>
2020-05-21 21:02:19 +0545

    a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import Login from './components/login'
-import Register from './components/register'
-import Dashboard from './components/dashboard'
+import React, { useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default class App extends Component {
+import { history } from '../helpers';
+import { alertActions } from '../actions';
+import { PrivateRoute } from '../components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
 
-  render() {
+function App() {
+    const alert = useSelector(state => state.alert);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }, []);
+
     return (
-      <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/dashboard" exact component={Dashboard} />
-        <Route>Page Not Found 404</Route>
-      </Switch>
-    </Router>
-    )
-  }
+        <div className="jumbotron">
+            <div className="container">
+                <div className="col-md-8 offset-md-2">
+                    {alert.message &&
+                        <div className={`alert ${alert.type}`}>{alert.message}</div>
+                    }
+                    <Router history={history}>
+                        <Switch>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/login" component={LoginPage} />
+                            <Route path="/register" component={RegisterPage} />
+                            <Redirect from="*" to="/" />
+                        </Switch>
+                    </Router>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export { App };
--- src/components/login.js
+++ src/components/login.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class login extends Component {
     render() {
         return (
-            <div>
-                this is login page
+            <div style={{margin:'100px'}}>
+                <input type="text" placeholder={"Enter your email"} /><br />
+                <input type="password" placeholder={"Enter your Password"} /><br />
+                <button>Login</button>
             </div>
         )
     }
