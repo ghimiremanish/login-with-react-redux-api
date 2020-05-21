2c28a15da9bdebde64afc3447af069e834ea61b2
Manish Ghimire <algorithm@Algorithm-2.local>
2020-05-21 18:59:04 +0545

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Login from './components/login'
+import Register from './components/register'
+import Dashboard from './components/dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route>Page Not Found 404</Route>
+      </Switch>
+    </Router>
+    )
+  }
 }
-
-export default App;
